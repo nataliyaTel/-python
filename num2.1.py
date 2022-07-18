a02a2d881466425d46364125dda443ee5273ef29
nataliyaTel <septem79@mail.ru>
2022-07-18 10:47:56 +0300

    num2.1.

---
+++ num2.1.py
@@ -0,0 +1,9 @@
+#Напишите программу, которая принимает на вход вещественное число и показывает сумму его цифр.
+#Пример:
+# 6782 -> 23
+# 0,56 -> 11
+def func(param_1,param_2,param_3,param_4):
+    summ=param_1+param_2+param_3+param_4
+    return summ
+print(func(6,7,8,2))    
+print(func(0,0,5,6))
